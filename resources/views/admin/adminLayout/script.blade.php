53fb91aa3f210b797c2fa9f2145c3769f7015255
icydev1 <neejurajpoot15@gmail.com>
2022-11-18 01:14:48 +0530

    push

--- resources/views/admin/adminLayout/script.blade.php
+++ resources/views/admin/adminLayout/script.blade.php
@@ -1,3 +1,5 @@
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <script src="{{asset('/assets/js/core/popper.min.js')}}"></script>
   <script src="{{asset('/assets/js/core/bootstrap.min.js')}}"></script>
   <script src="{{asset('/assets/js/plugins/perfect-scrollbar.min.js')}}"></script>
